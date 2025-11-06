b95a35bb31dc74d5f737546f52bd5f6345127ff2
Pamela Masters <pamela@schemanest.com>
2025-11-06 18:53:08 +0000

    unit test for workout

--- models/marts/workout.sql
+++ models/marts/workout.sql
@@ -4,15 +4,23 @@
   )
 }}
 
-SELECT  r.workout_id,
-        r.workout_timestamp,
-        r.length_minutes,
-        r.class_title,
-        r.total_output,
-        r.avg_watts,
-        r.distance_km,
-        r.calories_burned,
-        r.avg_heartrate_bpm,
-        r.fitness_discipline,
-        r.instructor
-FROM {{ ref('stg_workout') }} AS r
+SELECT workout_id,
+       workout_timestamp,
+       DATE(workout_timestamp) AS workout_date,
+       fitness_discipline,
+       instructor,
+       length_minutes,
+       total_output,
+       ROUND((total_output / NULLIF(length_minutes, 0)), 2)AS output_per_min,
+       distance_km,
+       calories_burned,
+       avg_heartrate_bpm,
+       ROUND((distance_km / NULLIF(length_minutes / 60.0, 0)), 2) AS avg_speed_kmh,
+       CASE
+         WHEN (total_output / NULLIF(length_minutes, 0)) IS NULL THEN 'n/a'
+         WHEN (total_output / NULLIF(length_minutes, 0)) < 6 THEN 'Low'
+         WHEN (total_output / NULLIF(length_minutes, 0)) <= 10 THEN 'Moderate'
+         ELSE 'High'
+       END AS intensity_band,
+       NULLIF(avg_watts, 0) AS avg_watts
+FROM {{ ref('stg_workout') }}
